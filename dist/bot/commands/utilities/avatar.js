d21f37f34e965ce032d787d548312e907669790e
Bailey Herbert <bailey.herbert@hotmail.com>
2019-02-24 12:11:58 -0700

    Add avatar command

--- dist/bot/commands/utilities/avatar.js
+++ dist/bot/commands/utilities/avatar.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const _api_1 = require("@api");
+const request = require("request");
+const emoji_1 = require("@bot/libraries/emoji");
 class Avatar extends _api_1.Command {
     constructor() {
         super({
@@ -16,9 +18,26 @@ class Avatar extends _api_1.Command {
             ]
         });
     }
-    execute(input) {
+    async execute(input) {
         let user = input.getArgument('user');
-        input.channel.send('Not yet implemented.');
+        let promise = new Promise((resolve, reject) => {
+            request(user.user.avatarURL, { encoding: null }, function (err, response, buffer) {
+                if (err)
+                    return reject(err);
+                resolve(buffer);
+            });
+        });
+        let message = await input.channel.send(`${emoji_1.Emoji.LOADING}  Retrieving avatar...`);
+        try {
+            input.channel.send({
+                file: await promise
+            });
+            await message.delete();
+        }
+        catch (err) {
+            await message.edit(`${emoji_1.Emoji.ERROR}  Failed to download avatar, try again in a few moments.`);
+            message.deleteAfter(6000);
+        }
     }
 }
 exports.Avatar = Avatar;
--- src/bot/commands/utilities/avatar.ts
+++ src/bot/commands/utilities/avatar.ts
@@ -1,5 +1,8 @@
 import { Command, Input } from '@api';
 import { GuildMember } from 'discord.js';
+import * as request from 'request';
+import { Emoji } from '@bot/libraries/emoji';
+import { Message } from 'discord.js';
 
 export class Avatar extends Command {
     constructor() {
@@ -17,9 +20,28 @@ export class Avatar extends Command {
         });
     }
 
-    execute(input: Input) {
+    async execute(input: Input) {
         let user = input.getArgument('user') as GuildMember;
+        let promise : Promise<Buffer> = new Promise((resolve, reject) => {
+            request(user.user.avatarURL, { encoding: null}, function(err, response, buffer) {
+                if (err) return reject(err);
+                resolve(buffer);
+            });
+        });
 
-        input.channel.send('Not yet implemented.');
+        // Post a loading message
+        let message = await input.channel.send(`${Emoji.LOADING}  Retrieving avatar...`) as Message;
+
+        // Get the buffer
+        try {
+            input.channel.send({
+                file: await promise
+            });
+            await message.delete();
+        }
+        catch (err) {
+            await message.edit(`${Emoji.ERROR}  Failed to download avatar, try again in a few moments.`);
+            message.deleteAfter(6000);
+        }
     }
 }
