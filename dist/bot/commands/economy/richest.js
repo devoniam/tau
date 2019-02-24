69d71be3ece23a4f54e479fe0e4e5072b9bc3877
Bailey Herbert <bailey.herbert@hotmail.com>
2019-02-24 12:05:05 -0700

    Add richest command

--- dist/bot/commands/economy/richest.js
+++ dist/bot/commands/economy/richest.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const _api_1 = require("@api");
+const economy_1 = require("@bot/libraries/economy");
+const Table = require('easy-utf8-table');
 class Richest extends _api_1.Command {
     constructor() {
         super({
@@ -18,9 +20,18 @@ class Richest extends _api_1.Command {
             ]
         });
     }
-    execute(input) {
+    async execute(input) {
         let limit = input.getArgument('limit');
-        input.channel.send('Not yet implemented.');
+        let rows = await economy_1.Economy.getAllBalances(input.guild);
+        let display = _.slice(rows, 0, limit);
+        let table = new Table();
+        display.forEach((row, i) => {
+            table.cell('#', i + 1);
+            table.cell('Member', row.member.displayName);
+            table.cell('Balance', `$${row.balance.toFixed(2)}`);
+            table.newRow();
+        });
+        input.channel.send('```\n' + table + '\n```');
     }
 }
 exports.Richest = Richest;
--- src/bot/commands/economy/richest.ts
+++ src/bot/commands/economy/richest.ts
@@ -1,4 +1,6 @@
 import { Command, Input } from '@api';
+import { Economy } from '@bot/libraries/economy';
+const Table = require('easy-utf8-table');
 
 export class Richest extends Command {
     constructor() {
@@ -18,9 +20,20 @@ export class Richest extends Command {
         });
     }
 
-    execute(input: Input) {
+    async execute(input: Input) {
         let limit = input.getArgument('limit') as number;
+        let rows = await Economy.getAllBalances(input.guild);
+        let display = _.slice(rows, 0, limit);
 
-        input.channel.send('Not yet implemented.');
+        let table = new Table();
+
+        display.forEach((row, i) => {
+            table.cell('#', i + 1);
+            table.cell('Member', row.member.displayName);
+            table.cell('Balance', `$${row.balance.toFixed(2)}`);
+            table.newRow();
+        });
+
+        input.channel.send('```\n' + table + '\n```');
     }
 }
