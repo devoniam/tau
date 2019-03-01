0f44c35eb6f2c97412f8af9198ea3ddd42f1fc1b
Jordan <Jdevelops897@gmail.com>
2019-03-01 16:31:46 -0700

    Add lastfm get command

--- dist/bot/commands/lastfm/fm.js
+++ dist/bot/commands/lastfm/fm.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const _api_1 = require("@api");
+const request = require("request");
+const unescape = require('unescape');
 class LastFm extends _api_1.Command {
     constructor() {
         super({
@@ -18,7 +20,6 @@ class LastFm extends _api_1.Command {
                 }
             ]
         });
-        this.key = '87aa68ded7b81dc193520b678aff7da6';
     }
     async execute(input) {
         let db = input.member.settings;
@@ -27,9 +28,65 @@ class LastFm extends _api_1.Command {
         if (!user) {
             user = db.lastfmId;
         }
+        let key = '87aa68ded7b81dc193520b678aff7da6';
+        let lastfmURL = 'http://ws.audioscrobbler.com/2.0/?method=';
+        let queryString = '&user=' + user + '&api_key= ' + key + '&limit=2&format=json';
         switch (action) {
             case 'get':
-                if (!user && user != '') {
+                if (user && user != '') {
+                    let requestURL = request((lastfmURL + 'user.getRecentTracks' + queryString), (error, response, body) => {
+                        if (error) {
+                            input.channel.send({
+                                embed: {
+                                    color: 3447003,
+                                    title: 'Connection Error',
+                                    description: "Unable to retrieve lastfm data"
+                                }
+                            });
+                        }
+                        let nullText = '[undefined]';
+                        let parsed = JSON.parse(body);
+                        let currentTrack = parsed.recenttracks.track[0];
+                        let trackName = currentTrack.name;
+                        let artistName = currentTrack.artist['#text'];
+                        let albumName = currentTrack.album['#text'];
+                        let albumImage = currentTrack.image[1]['#text'];
+                        let lastTrack = parsed.recenttracks.track[1];
+                        let lastTrackName = lastTrack.name;
+                        let lastArtistName = lastTrack.artist['#text'];
+                        let lastAlbumName = lastTrack.album['#text'];
+                        let description = 'Recently Played:';
+                        let prefix = 'Last track:';
+                        if (currentTrack.IsNowPlaying) {
+                            description = 'Now Playing:';
+                            prefix = 'Current:';
+                        }
+                        input.channel.send({
+                            embed: {
+                                color: 3447003,
+                                author: {
+                                    name: user,
+                                    icon_url: input.member.user.avatarURL
+                                },
+                                title: user + ', ' + input.member.displayName,
+                                url: 'https://www.last.fm/user/' + user,
+                                description: description,
+                                thumbnail: {
+                                    url: albumImage
+                                },
+                                fields: [
+                                    {
+                                        name: prefix + ' ' + trackName,
+                                        value: artistName + ' | ' + albumName
+                                    },
+                                    {
+                                        name: 'Previous: ' + lastTrackName,
+                                        value: lastArtistName + ' | ' + lastAlbumName
+                                    }
+                                ]
+                            }
+                        });
+                    });
                 }
                 break;
             case 'set':
--- src/bot/commands/lastfm/fm.ts
+++ src/bot/commands/lastfm/fm.ts
@@ -1,8 +1,9 @@
 import { Command, Input } from '@api';
+import * as request from 'request';
+import { Response } from 'request';
+const unescape = require('unescape');
 
 export class LastFm extends Command {
-
-    private key: string = '87aa68ded7b81dc193520b678aff7da6';
 
     constructor() {
         super({
@@ -27,15 +28,74 @@ export class LastFm extends Command {
 
         let action = input.getArgument('action') as string;
         let user = input.getArgument('user') as string | undefined;
+        if (!user) { user = db.lastfmId; }
 
-        if (!user) {
-            user = db.lastfmId;
-        }
+        let key = '87aa68ded7b81dc193520b678aff7da6';
+        let lastfmURL = 'http://ws.audioscrobbler.com/2.0/?method=';
+        let queryString = '&user=' + user + '&api_key= ' + key + '&limit=2&format=json';
 
         switch(action) {
             case 'get':
-                if (!user && user != '') {
+                if (user && user != '') {
+                    let requestURL = request((lastfmURL + 'user.getRecentTracks' + queryString), (error: any, response: Response, body: any) => {
+                        if (error) {
+                            input.channel.send({
+                                embed: {
+                                    color: 3447003,
+                                    title: 'Connection Error',
+                                    description: "Unable to retrieve lastfm data"
+                                }
+                            });
+                        }
 
+                        let nullText = '[undefined]';
+                        let parsed = JSON.parse(body);
+                        let currentTrack = parsed.recenttracks.track[0];
+
+                        let trackName = currentTrack.name;
+                        let artistName = currentTrack.artist['#text'];
+                        let albumName = currentTrack.album['#text'];
+                        let albumImage = currentTrack.image[1]['#text'];
+
+                        let lastTrack = parsed.recenttracks.track[1];
+
+                        let lastTrackName = lastTrack.name;
+                        let lastArtistName = lastTrack.artist['#text'];
+                        let lastAlbumName = lastTrack.album['#text'];
+
+                        let description = 'Recently Played:';
+                        let prefix = 'Last track:';
+                        if (currentTrack.IsNowPlaying) {
+                            description = 'Now Playing:';
+                            prefix = 'Current:';
+                        }
+                        input.channel.send( {
+                            embed: 
+                            {
+                                color: 3447003,
+                                author: {
+                                    name: user,
+                                    icon_url: input.member.user.avatarURL
+                                },
+                                title: user + ', ' + input.member.displayName,
+                                url:  'https://www.last.fm/user/' + user,
+                                description: description,
+                                thumbnail: {
+                                    url: albumImage
+                                },
+                                fields: [
+                                    {
+                                        name: prefix + ' ' + trackName,
+                                        value: artistName + ' | ' + albumName
+                                    },
+                                    {
+                                        name: 'Previous: ' + lastTrackName,
+                                        value: lastArtistName + ' | ' + lastAlbumName
+                                    }
+                                ]
+                            }
+                        });
+                    });
                 }
                 break;
             case 'set':
