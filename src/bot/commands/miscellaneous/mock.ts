2b14f82de61525940cb9a460214feeaf889ce465
Bailey Herbert <bailey.herbert@hotmail.com>
2019-03-06 16:59:07 -0700

    Migrate mock command

--- src/bot/commands/miscellaneous/mock.ts
+++ src/bot/commands/miscellaneous/mock.ts
@@ -1,5 +1,6 @@
 import { Command, Input } from '@api';
 import { GuildMember } from 'discord.js';
+import { Emoji } from '@bot/libraries/emoji';
 
 export class Mock extends Command {
     constructor() {
@@ -16,11 +17,35 @@ export class Mock extends Command {
         });
     }
 
-    execute(input: Input) {
+    async execute(input: Input) {
         let user = input.getArgument('user') as GuildMember;
         let lastMessage = user.lastMessage;
-        let lastMessageText = lastMessage.content;
 
-        input.channel.send('Not yet implemented.');
+        if (user == input.member) {
+            return await input.channel.send(`${Emoji.ERROR} You can't mock yourself!`);
+        }
+
+        if (lastMessage && lastMessage.content.charAt(0) != input.guild.settings.prefix) {
+            let lastMessageText = lastMessage.content;
+            let changed = "";
+
+            for (let i = 0; i < lastMessageText.length; i++) {
+                let char = lastMessageText[i];
+
+                if (i % 2 === 0) {
+                    char = char.toUpperCase();
+                }
+                else {
+                    char = char.toLowerCase();
+                }
+
+                changed += char;
+            }
+
+            await input.channel.send(changed);
+        }
+        else {
+            return await input.channel.send(`${Emoji.ERROR} Can't find a message to mock!`);
+        }
     }
 }
