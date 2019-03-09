ad086d2d934396d8ae88244d8a4528aaed030de9
devond <devon.digia@gmail.com>
2019-03-08 17:28:38 -0700

    Update match.ts
    Added function that finds the next lowest price to the product being matched.

--- src/bot/commands/price-matching/match.ts
+++ src/bot/commands/price-matching/match.ts
@@ -22,27 +22,67 @@ export class Match extends Command {
     execute(input: Input) {
         let url = input.getArgument('url') as string;
 
+        /* Return Amazon */
         request(url, { gzip: true }, function(a, b, body){
-            /*load html of web page*/
+          
+            // load html of web page
             let $ = cheerio.load(body);
 
-            /*scrapes product title from product page*/
-            let item_text = $('#productTitle').text().trim();
-
-            /*combines the encoded item_text and throws it into amazon's search URL*/
+            // Title of product being matched
+            let item_text = $('#productTitle').text().trim(); 
+            // Price of product being matched
+            let item_price = Math.floor(parseFloat($('#priceblock_ourprice').text().trim().replace('$', '')));    
+            
+            // Combines the encoded item_text and throws it into amazon's search URL
             let amazon_search = 'https://www.amazon.com/s?k=' + encodeURI(item_text);
             let amazon = 'https://www.amazon.com';
+
+            /* Return Amazon */
             request(amazon_search, { gzip: true }, (a2, b2, body2) => {
                 let $$ = cheerio.load(body2);
-
-                //let link = $( "h5.a-color-base a" ).first().attr('href');
                 let link = $$('.s-result-list .a-link-normal').first().attr('href');
-                //console.log(link);
-                //console.log($$('.s-result-list .a-link-normal').first());
 
                 input.channel.send(amazon + link);
 
-                console.log(amazon_search);
+                // console.log(amazon_search); 
+                
+                // Get all rows
+                let rows = $$('.a-section');
+                let prices : number[] = [];
+
+                // Iterate through each row
+                rows.each((i, row) => {
+                    // Get the first <h5> in the row
+                    let h5 = $$(row).find('h5').first();
+
+                    // Get the first class="a-price-whole" in the row (has price)
+                    let whole = $$(row).find('.a-price-whole').first();
+
+                    // If h5 or price is missing, skip
+                    if (h5.length == 0) return;
+                    if (whole.length == 0) return;
+
+                    // Extract text from each
+                    let title = h5.text().trim();
+                    let price_whole = parseInt(whole.text().trim().replace('.', ''));
+                    
+                    // Place prices into an array
+                    if (price_whole < item_price) {
+                        prices.push(price_whole);
+                    }
+
+                    console.log(title, '$' + price_whole);
+                });
+
+                // Find next lowest
+                prices = prices.sort((a, b) => {
+                    return b - a;
+                });
+
+                console.log('sorted:', prices)
+
+                let nextLowestPrice = prices[0];
+                console.log('next lowest:', nextLowestPrice)
             });
 
         });
