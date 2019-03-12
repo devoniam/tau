e35fe43d83b61854ed822190a2b863b955eff8db
Bailey Herbert <bailey.herbert@hotmail.com>
2019-03-12 11:59:23 -0700

    Remove chat logging

--- src/framework/internal/server.ts
+++ src/framework/internal/server.ts
@@ -1,7 +1,6 @@
 import * as SocketIO from 'socket.io';
 import { Server as SocketServer } from 'socket.io';
 import { Framework } from '@core/framework';
-import { ChatLogStatus } from '@bot/listeners/chatlog';
 
 export class Server {
 
@@ -44,11 +43,6 @@ export class Server {
                         channel.send(`:loudspeaker:  ${message}`);
                     }
                 }
-            });
-
-            // Chat logging
-            socket.on('chat', (enabled: string) => {
-                ChatLogStatus.active = (enabled.equalsIgnoreCase('on') || enabled.equalsIgnoreCase('yes'));
             });
 
             // Stop the bot
